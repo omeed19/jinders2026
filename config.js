7aaa19574c0f1e5cab3350f596fd2907a4c7982e
omeed <49353191+omeed19@users.noreply.github.com>
2026-02-10 12:35:52 -0800

    Update config.js

--- config.js
+++ config.js
@@ -68,7 +68,7 @@ const CONFIG = {
         musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.2                        // Volume level (0.0 to 1.0)
+        volume: 0                        // Volume level (0.0 to 1.0)
     }
 };
 
